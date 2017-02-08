fa70825fcef205d37bf48b9f026d6820ad04e539
caleb <caleb.e.allen@gmail.com>
2017-02-08 02:55:04 -0700

    use average of recent frames for noise reduction

--- app/src/main/java/io/caleballen/audiojam/MainActivity.java
+++ app/src/main/java/io/caleballen/audiojam/MainActivity.java
@@ -13,7 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.androidplot.xy.BarFormatter;
 import com.androidplot.xy.BoundaryMode;
-import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYPlot;
 import com.androidplot.xy.XYSeries;
@@ -21,6 +20,8 @@ import com.androidplot.xy.XYSeries;
 import org.jtransforms.fft.DoubleFFT_1D;
 
 import java.util.Arrays;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import timber.log.Timber;
 
@@ -28,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int SAMPLERATE = 44100;
     private static final int BUCKETS = 1024;
+    private static final int FRAMES_THRESHOLD = 4;
     //    private static final int SAMPLERATE = 8000;
     private static final int CHANNELS = AudioFormat.CHANNEL_IN_MONO;
     private static final int AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
@@ -36,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     private Thread recordingThread = null;
     private XYPlot plot;
     private short buffer[] = new short[BUCKETS];
+    private Queue<Double[]> bufferFrames;
+    private Double[] averages;
 
 
     @Override
@@ -46,18 +50,24 @@ public class MainActivity extends AppCompatActivity {
         plot = (XYPlot) findViewById(R.id.plot);
 //        plot.setRangeBoundaries(0, 22000, BoundaryMode.GROW);
         plot.setRangeLowerBoundary(0, BoundaryMode.FIXED);
-        plot.setRangeUpperBoundary(100000, BoundaryMode.FIXED);
+        plot.setRangeUpperBoundary(100, BoundaryMode.FIXED);
         plot.setDomainLowerBoundary(0, BoundaryMode.FIXED);
         plot.setDomainUpperBoundary(BUCKETS, BoundaryMode.FIXED);
 //        plot.setDomainUpperBoundary(300, BoundaryMode.FIXED);
 
 //        plot.setRenderMode(Plot.RenderMode.USE_BACKGROUND_THREAD);
 
+        bufferFrames = new LinkedBlockingQueue<>();
+        averages = new Double[BUCKETS];
+        for (int i = 0; i < averages.length; i++) {
+            averages[i] = 0D;
+        }
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 0);
         }
 
-        int bufferSize = AudioRecord.getMinBufferSize(SAMPLERATE, CHANNELS, AUDIO_ENCODING);
+        final int bufferSize = AudioRecord.getMinBufferSize(SAMPLERATE, CHANNELS, AUDIO_ENCODING);
         recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
                 SAMPLERATE, CHANNELS, AUDIO_ENCODING, bufferSize);
 
@@ -79,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
 
                     Double[] dData = new Double[data.length];
                     for (int i = 0; i < data.length; i++) {
-                        dData[i] = data[i];
+                        dData[i] = Math.abs(data[i]);
                     }
 
                     //high pass filter
@@ -88,11 +98,29 @@ public class MainActivity extends AppCompatActivity {
 //                        dData[i] = data[i + (data.length / 2)];
 //                    }
 
+                    if (bufferFrames.size() >= FRAMES_THRESHOLD) {
+                        Double[] frameToRemove = bufferFrames.poll();
+                        for (int i = 0; i < averages.length; i++) {
+                            averages[i] -= frameToRemove[i];
+                        }
+                    }
+                    if (bufferFrames.size() < FRAMES_THRESHOLD) {
+                        bufferFrames.add(dData);
+                        for (int i = 0; i < averages.length; i++) {
+                            averages[i] += dData[i];
+                        }
+                    }
 
-                    LineAndPointFormatter seriesFormat = new LineAndPointFormatter(Color.RED, Color.GREEN, Color.BLUE, null);
+                    Double[] calcAverages = new Double[BUCKETS];
+                    for (int i = 0; i < calcAverages.length; i++) {
+                        calcAverages[i] = averages[i] / averages.length;
+                    }
+
+//                    LineAndPointFormatter seriesFormat = new LineAndPointFormatter(Color.RED, Color.GREEN, Color.BLUE, null);
                     BarFormatter bf = new BarFormatter(Color.CYAN, Color.CYAN);
 
-                    XYSeries series = new SimpleXYSeries(Arrays.asList(dData), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Frequencies");
+                    XYSeries series = new SimpleXYSeries(Arrays.asList(calcAverages), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Frequencies");
+//                    XYSeries series = new SimpleXYSeries(Arrays.asList(dData), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Frequencies");
                     plot.clear();
                     plot.addSeries(series, bf);
                     plot.redraw();
